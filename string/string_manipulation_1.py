a1de909ee80af8945d778dec93f435665b38548c
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-08-28 09:54:10 +0900

    studied string manipulation

---
+++ string/string_manipulation_1.py
@@ -0,0 +1,14 @@
+# 文字列の連結
+print("ABC" + "DEF" + "GHI")
+
+string1 = "Hello"
+string2 = "Python"
+
+print(string1 + " " +  string2)
+
+
+# 文字列の繰り返し
+print("ABC" * 4)
+string3 = "Hello ! "
+n = 3
+print(string3 * n)
---
+++ string/string_manipulation_2.py
@@ -0,0 +1,7 @@
+# 数値を文字列に変換する。
+
+month = 5
+date = 14
+string = "Your birthday is "
+
+print(string + str(month) + "/" + str(date))
